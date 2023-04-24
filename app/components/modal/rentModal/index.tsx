c5148a974f84cd28774ca98c3a55bf43f164a56a
maritocuate <mario300@gmail.com>
2023-04-23 21:00:36 -0300

    chore: listing creation (price and submit function)

--- app/components/modal/rentModal/index.tsx
+++ app/components/modal/rentModal/index.tsx
@@ -1,18 +1,21 @@
 'use client'
 
 import { useCallback, useMemo, useState } from 'react'
+import dynamic from 'next/dynamic'
+import { useRouter } from 'next/navigation'
+import axios from 'axios'
 
 import Modal from '..'
 import Heading from '../../heading'
 import useRentModal from '../../hooks/useRentModal'
 import { categories } from '../../navbar/categories'
 import CategoryInput from '../../inputs/categoryInput'
-import { FieldValues, useForm } from 'react-hook-form'
+import { FieldValues, SubmitHandler, useForm } from 'react-hook-form'
 import CountrySelect from '../../inputs/countrySelect'
-import dynamic from 'next/dynamic'
 import Counter from '../../inputs/counter'
 import ImageUpload from '../../inputs/imageUpload'
 import Input from '../../inputs/input'
+import toast from 'react-hot-toast'
 
 enum STEPS {
     CATEGORY = 0,
@@ -24,6 +27,7 @@ enum STEPS {
 }
 
 const RentModal = () => {
+    const router = useRouter()
     const rentModal = useRentModal()
     const [step, setStep] = useState(STEPS.CATEGORY)
     const [loading, setLoading] = useState(false)
@@ -75,6 +79,25 @@ const RentModal = () => {
     }
     const onNext = () => {
         setStep(value => value+1)
+    }
+    const onSubmit: SubmitHandler<FieldValues> = (data) => {
+        if(step !== STEPS.PRICE) return onNext()
+        setLoading(true)
+
+        axios.post('../../api/listings', data)
+        .then(() => {
+            toast.success('Listing created!')
+            router.refresh()
+            reset()
+            setStep(STEPS.CATEGORY)
+            rentModal.onClose()
+        })
+        .catch(() => {
+            toast.error('Something went wrong')
+        })
+        .finally(() => {
+            setLoading(false)
+        })
     }
 
     const actionLabel = useMemo(() => {
@@ -213,16 +236,38 @@ const RentModal = () => {
         )
     }
 
+    if(step === STEPS.PRICE) {
+        bodyContent = (
+            <div className="flex flex-col gap-8">
+                <Heading
+                    title='Now, set your price'
+                    subtitle='How much do you charge per night?'
+                />
+                <Input
+                    id='price'
+                    label='Price'
+                    formatPrice
+                    type='number'
+                    disabled={loading}
+                    register={register}
+                    errors={errors}
+                    required
+                />
+            </div>
+        )
+    }
+
     return(
         <Modal
             isOpen={rentModal.isOpen}
             onClose={rentModal.onClose}
-            onSubmit={onNext}
+            onSubmit={handleSubmit(onSubmit)}
             actionLabel={actionLabel}
             secondaryActionLabel={secondaryActionLabel}
             secondaryAction={step === STEPS.CATEGORY ? undefined : onBack}
             title='Marbnb your home'
             body={bodyContent}
+            disabled={loading}
         />
     )
 }
